97e96aa31ca2816184f8623b6a522ce600e54d67
h591309 <h591309@stud.hvl.no>
2023-01-26 16:18:57 +0100

    Endret design av fragment_first.xml

--- app/src/main/java/com/example/oblig1/FirstFragment.java
+++ app/src/main/java/com/example/oblig1/FirstFragment.java
@@ -29,7 +29,15 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        binding.buttonQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        binding.buttonDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
